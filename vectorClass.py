ddcf61bd11be35afcbb535cd4ca189be3d5720e7
rafael001 <rafael.marrero4@upr.edu>
2017-06-18 14:21:11 -0400

    Falta el ejercicio 2
    Logre hacer ejercicios 1,3,4 y  5

--- vectorClass.py
+++ vectorClass.py
@@ -11,6 +11,8 @@ class Vector:
     "Class to manage vectors (unidimensional arrays) of floating-point numbers"
 
     def __init__(self, size):
+        """Constructor of vector. If size <= 0 IllegalSize ir raised
+        Initialy the vector has all elements as zero"""
         try:
             if size < 1:
                 raise IllegalSize
@@ -22,9 +24,12 @@ class Vector:
             print("Size must be greater or equal to 1")
         
     def getSize(self):
+        "Method that returns the size of the vector"
         return self.size
 
     def get(self, i):
+        """Method that returns the element of a vector at position i
+        If i>=self.size or i<0, IllegalIndex is called"""
         try:
             if i < 0 or i >= self.size :
                 raise IllegalIndex
@@ -34,6 +39,9 @@ class Vector:
             print("Index out of range")
 
     def set(self, i, value):
+        """Method that mutates the element of a vector at position i
+        with that of value. If i>=self.size or i<0,
+        IllegalIndex is called"""
         try:
             if i < 0 or i >= self.size :
                 raise IllegalIndex
@@ -43,6 +51,9 @@ class Vector:
             print("Index out of range")
 
     def __add__(self, other):
+        """Overloading the  + operator to compute the sum of the 2 vectors
+        (self+other). If the vectors have different sizes, SizeMismatch
+        exception is raised"""
         try:
             if self.size != other.size:
                 raise SizeMismatch
@@ -55,6 +66,9 @@ class Vector:
             print("Vectors have different sizes")
             
     def __sub__(self, other):
+        """Overloading the  - operator to compute the substraction of the 2 vectors
+        (self-other). If the vectors have different sizes, SizeMismatch
+        exception is raised"""
         try:
             if self.size != other.size:
                 raise SizeMismatch
@@ -67,18 +81,29 @@ class Vector:
             print("Vectors have different sizes")
 
     def __mul__(self, other):
-        try:
-            if self.size != other.size:
-                raise SizeMismatch
-            else:
-                dotProduct = 0
-                for i in range(self.size):
-                   dotProduct += self.get(i) * other.get(i)
-            return dotProduct
-        except SizeMismatch:
-            print("Vectors have different sizes")
+        """Overloading the  * operator to compute the dot-product of the 2 vectors
+        (self*other). If the vectors have different sizes, SizeMismatch
+        exception is raised"""
+        if type(other) == float:
+            v = Vector(self.size)
+            for i in range(self.size):
+                v.set(i, self.get(i)*other)
+            return v
+        else:
+            try:
+                if self.size != other.size:
+                    raise SizeMismatch
+                else:
+                    dotProduct = 0
+                    for i in range(self.size):
+                       dotProduct += self.get(i) * other.get(i)
+                return dotProduct
+            except SizeMismatch:
+                print("Vectors have different sizes")
 
     def __eq__(self, other):
+        """Overloading the  == operator to compare 2 vectors (self==other).
+        If the vectors have different sizes, SizeMismatch exception is raised"""
         try:
             if self.size != other.size:
                 raise SizeMismatch
@@ -92,6 +117,8 @@ class Vector:
             print("Vectors have different sizes")
 
     def __ne__(self, other):
+        """Overloading the  != operator to compare 2 vectors (self!=other).
+        If the vectors have different sizes, SizeMismatch exception is raised"""
         try:
             if self.size != other.size:
                 raise SizeMismatch
@@ -108,18 +135,69 @@ class Vector:
             print("Vectors have different sizes")
 
     def __pos__(self):
+        "Overloading operator + to return a copy of the vector (+self)"
         v = Vector(self.size)
         for i in range(self.size):
              v.set(i, self.get(i))
         return v
 
     def __neg__(self):
+        """Overloading the - operator to return a copy of the vector
+        where the signs of the elements are changed (-self)."""
         v = Vector(self.size)
         for i in range(self.size):
              v.set(i, -1 * self.get(i))
         return v
 
+    def __iadd__(self, other):
+        """Overloading the  += operator to compute the substraction of the 2 vectors and
+        storing it in self (self+=other). If the vectors have different sizes, SizeMismatch
+        exception is raised"""
+        try:
+            if self.size != other.size:
+                raise SizeMismatch
+            else:
+                for i in range(self.size):
+                    self.set(i, self.get(i) + other.get(i))
+                return self
+        except SizeMismatch:
+            print("Vectors have different sizes")
+
+    def __isub__(self, other):
+        """Overloading the  -= operator to compute the substraction of the 2 vectors and
+        storing it in self (self-=other). If the vectors have different sizes, SizeMismatch
+        exception is raised"""
+        try:
+            if self.size != other.size:
+                raise SizeMismatch
+            else:
+                for i in range(self.size):
+                    self.set(i, self.get(i) - other.get(i))
+                return self
+        except SizeMismatch:
+            print("Vectors have different sizes")
+
+    def __imul__(self, other):
+        """Overloading the  *= operator to compute the scalar multiplication of a vector and
+        storing it in self (self-=other). If the vectors have different sizes, SizeMismatch
+        exception is raised"""
+        if type(other) == float:
+            for i in range(self.size):
+                self.set(i, self.get(i)*other)
+            return self
+        else:
+            print("In-place multiplication must be between vector and a float")
+
+    def __getitem__(self, i):
+        "Method that calls self.get(i) using the [] operator"
+        return self.get(i)
+
+    def __setitem__(self, i, value):
+        "Method that calls self.set(i, value) using the [] operator"
+        self.set(i, value)
+    
     def min(self):
+        "Method that returns the minimum of the vector"
         """
         minimum = self.get(0)
         for i in range(1,self.size):
@@ -130,6 +208,7 @@ class Vector:
         return min(self.vector)
     
     def max(self):
+        "Method that returns the maximum of the vector"
         """
         maximum = self.get(0)
         for i in range(1,self.size):
@@ -140,10 +219,14 @@ class Vector:
         return max(self.vector)
     
     def sum(self):
+        "Method that returns the sum of the elements of the vector"
         total = 0
         for i in range(self.size):
             total += self.get(i)
         return total
     
     def average(self):
+        "Method that returns the average of the elements of the vector"
         return self.sum()/self.getSize()
+
+
